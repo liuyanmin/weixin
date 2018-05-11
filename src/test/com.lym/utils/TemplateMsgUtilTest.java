a3ac6637798215ee6ba795f82f12a38b5f7f8565
liuyanmin <liuyanmin@kingsoft.com>
2018-05-11 14:58:16 +0800

    增加消息模板测试类和.ignore文件

--- src/test/com.lym/utils/TemplateMsgUtilTest.java
+++ src/test/com.lym/utils/TemplateMsgUtilTest.java
@@ -18,12 +18,61 @@ public class TemplateMsgUtilTest {
     }
 
     /**
+     * 设置行业信息接口测试
+     */
+    @Test
+    public void testSetIndustry() {
+        String result = TemplateMsgUtil.setIndustry("16", "17", accessToken);
+        System.out.println(result);
+    }
+
+
+    /**
+     * 获取设置的行业信息接口测试
+     */
+    @Test
+    public void testGetIndustry() {
+        String result = TemplateMsgUtil.getIndustry(accessToken);
+        System.out.println(result);
+    }
+
+
+    /**
      * 获得模板ID接口测试
      */
     @Test
     public void testGetTemplateId() {
         String result = TemplateMsgUtil.getTemplateId("OPENTM200851401", accessToken);
         System.out.println(result);
+    }
+
+
+    /**
+     * 获取模板列表接口测试
+     */
+    @Test
+    public void testGetTemplateInfoList() {
+        String result = TemplateMsgUtil.getTemplateInfoList(accessToken);
+        System.out.println(result);
+    }
+
+
+    /**
+     * 删除模板接口测试
+     */
+    @Test
+    public void testDeleteTemplate() {
+        String result = TemplateMsgUtil.deleteTemplate("banjt_OxMOw0GGGRjmTe6JvpX4eFE0Rq82XNL4DMXOY", accessToken);
+        System.out.println(result);
+    }
+
+
+    /**
+     * 一次性订阅消息接口测试
+     */
+    @Test
+    public void testSendOneTimeTemplate() {
+
     }
 
 }
